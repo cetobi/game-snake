ac13fbd798953997d3ce856819a2184ee75d4dbd
cetobi <andre.marcdias@gmail.com>
2021-03-06 17:18:51 -0300

    Adicionar comida no jogo

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillStyle = "gray";
+    context.fillStyle = "DarkSlateGray";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
@@ -18,6 +22,11 @@ function criarCobrinha() {
         context.fillStyle = "white";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "CadetBlue";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -37,6 +46,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
